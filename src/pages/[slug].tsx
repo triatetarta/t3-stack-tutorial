f4dd64cb6e51d81ccb0e85ab6a420502fc50303b
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2023-03-23 23:26:52 +0000

    single post page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,14 +1,11 @@
 import type { NextPage, GetStaticProps } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
-import { createProxySSGHelpers } from "@trpc/react-query/ssg";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
-import superjson from "superjson";
 import Layout from "~/components/Layout";
 import Image from "next/image";
 import { LoadingPage } from "~/components/Loading";
 import PostView from "~/components/PostView";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
 const ProfileFeed = (props: { userId: string }) => {
   const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
@@ -62,11 +59,7 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = createProxySSGHelpers({
-    router: appRouter,
-    ctx: { prisma, userId: null },
-    transformer: superjson, // optional - adds superjson serialization
-  });
+  const ssg = generateSSGHelper();
 
   const slug = context.params?.slug;
 
