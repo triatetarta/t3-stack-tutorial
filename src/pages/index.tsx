26c9ba0e40d3df21711172da2ce85198225c9093
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2023-03-23 19:45:46 +0000

    router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -17,8 +17,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!user.isSignedIn && <SignInButton />}
-        {!!user.isSignedIn && <SignOutButton />}
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => {
+            return <div key={post.id}>{post.content}</div>;
+          })}
+        </div>
       </main>
     </>
   );
